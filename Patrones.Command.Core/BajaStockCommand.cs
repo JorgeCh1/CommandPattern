3e65d76e79584592bf4df4cb2c3a4aa8235c6402
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-03-29 14:49:39 -0600

    Algunas cosillas

--- Patrones.Command.Core/BajaStockCommand.cs
+++ Patrones.Command.Core/BajaStockCommand.cs
@@ -3,7 +3,7 @@ namespace Patrones.Command.Core
 {
     public class BajaStockCommand : OrdenCommand
     {
-        // Constructor para realiazar la agregación
+        // Constructor para realizar la agregación
         public BajaStockCommand(ProductoReceiver producto, double cantidad) : base(producto, cantidad)
         {
         }
